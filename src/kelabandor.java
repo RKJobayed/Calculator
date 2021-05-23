5a784d73719a7ecd715f3b0055f5ff0c2c820dce
RKJobayed <83612449+RKJobayed@users.noreply.github.com>
2021-05-23 13:22:45 +0600

    j
    design changed a little bit

--- src/kelabandor.java
+++ src/kelabandor.java
@@ -13,10 +13,11 @@ import java.awt.Window.Type;
 import java.awt.Button;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Toolkit;
 
 public class kelabandor {
 
-	private JFrame frame;
+	private JFrame frmKelaBandorCalculator;
 	double first;
 	double second;
 	double result;
@@ -31,7 +32,7 @@ public class kelabandor {
 			public void run() {
 				try {
 					kelabandor window = new kelabandor();
-					window.frame.setVisible(true);
+					window.frmKelaBandorCalculator.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -50,13 +51,15 @@ public class kelabandor {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setForeground(Color.GREEN);
-		frame.getContentPane().setFont(new Font("Tahoma", Font.BOLD, 11));
-		frame.getContentPane().setBackground(Color.BLACK);
-		frame.setBounds(100, 100, 494, 360);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmKelaBandorCalculator = new JFrame();
+		frmKelaBandorCalculator.setTitle("Kela Bandor Calculator");
+		frmKelaBandorCalculator.setIconImage(Toolkit.getDefaultToolkit().getImage("G:\\LoGOs\\Name Logo Mini.png"));
+		frmKelaBandorCalculator.getContentPane().setForeground(Color.GREEN);
+		frmKelaBandorCalculator.getContentPane().setFont(new Font("Tahoma", Font.BOLD, 11));
+		frmKelaBandorCalculator.getContentPane().setBackground(Color.BLACK);
+		frmKelaBandorCalculator.setBounds(100, 100, 494, 360);
+		frmKelaBandorCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmKelaBandorCalculator.getContentPane().setLayout(null);
 		
 		JTextArea textArea = new JTextArea();
 		textArea.setLineWrap(true);
@@ -64,7 +67,7 @@ public class kelabandor {
 		textArea.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		textArea.setBackground(new Color(192, 192, 192));
 		textArea.setBounds(10, 11, 375, 136);
-		frame.getContentPane().add(textArea);
+		frmKelaBandorCalculator.getContentPane().add(textArea);
 		
 		JButton btnNewButton = 
 				new JButton("1");
@@ -79,7 +82,7 @@ public class kelabandor {
 		btnNewButton.setForeground(Color.CYAN);
 		btnNewButton.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton.setBounds(10, 158, 60, 40);
-		frame.getContentPane().add(btnNewButton);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("4");
 		btnNewButton_1.addActionListener(new ActionListener() {
@@ -92,7 +95,7 @@ public class kelabandor {
 		btnNewButton_1.setForeground(Color.MAGENTA);
 		btnNewButton_1.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton_1.setBounds(10, 209, 60, 40);
-		frame.getContentPane().add(btnNewButton_1);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("7");
 		btnNewButton_2.addActionListener(new ActionListener() {
@@ -105,7 +108,7 @@ public class kelabandor {
 		btnNewButton_2.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton_2.setBackground(Color.ORANGE);
 		btnNewButton_2.setBounds(10, 260, 60, 50);
-		frame.getContentPane().add(btnNewButton_2);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("8");
 		btnNewButton_3.addActionListener(new ActionListener() {
@@ -118,7 +121,7 @@ public class kelabandor {
 		btnNewButton_3.setBackground(Color.ORANGE);
 		btnNewButton_3.setForeground(Color.GREEN);
 		btnNewButton_3.setBounds(80, 260, 60, 50);
-		frame.getContentPane().add(btnNewButton_3);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("5");
 		btnNewButton_4.addActionListener(new ActionListener() {
@@ -131,7 +134,7 @@ public class kelabandor {
 		btnNewButton_4.setBackground(Color.ORANGE);
 		btnNewButton_4.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton_4.setBounds(80, 209, 60, 40);
-		frame.getContentPane().add(btnNewButton_4);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_4);
 		
 		JButton btnNewButton_5 = new JButton("2");
 		btnNewButton_5.addActionListener(new ActionListener() {
@@ -144,7 +147,7 @@ public class kelabandor {
 		btnNewButton_5.setForeground(Color.BLACK);
 		btnNewButton_5.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton_5.setBounds(80, 158, 60, 40);
-		frame.getContentPane().add(btnNewButton_5);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_5);
 		
 		JButton btnNewButton_6 = new JButton("9");
 		btnNewButton_6.addActionListener(new ActionListener() {
@@ -157,7 +160,7 @@ public class kelabandor {
 		btnNewButton_6.setForeground(Color.DARK_GRAY);
 		btnNewButton_6.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton_6.setBounds(150, 260, 60, 50);
-		frame.getContentPane().add(btnNewButton_6);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_6);
 		
 		JButton btnNewButton_7 = new JButton("6");
 		btnNewButton_7.addActionListener(new ActionListener() {
@@ -170,7 +173,7 @@ public class kelabandor {
 		btnNewButton_7.setBackground(Color.ORANGE);
 		btnNewButton_7.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton_7.setBounds(150, 209, 60, 40);
-		frame.getContentPane().add(btnNewButton_7);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_7);
 		
 		JButton btnNewButton_8 = new JButton("3");
 		btnNewButton_8.addActionListener(new ActionListener() {
@@ -183,7 +186,7 @@ public class kelabandor {
 		btnNewButton_8.setBackground(Color.ORANGE);
 		btnNewButton_8.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton_8.setBounds(150, 158, 60, 40);
-		frame.getContentPane().add(btnNewButton_8);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_8);
 		
 		JButton btnNewButton_9 = new JButton("0");
 		btnNewButton_9.addActionListener(new ActionListener() {
@@ -196,7 +199,7 @@ public class kelabandor {
 		btnNewButton_9.setBackground(Color.ORANGE);
 		btnNewButton_9.setFont(new Font("Imprint MT Shadow", Font.BOLD, 26));
 		btnNewButton_9.setBounds(221, 158, 48, 152);
-		frame.getContentPane().add(btnNewButton_9);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_9);
 		
 		JButton btnNewButton_10 = new JButton("+");
 		btnNewButton_10.addActionListener(new ActionListener() {
@@ -210,7 +213,7 @@ public class kelabandor {
 		btnNewButton_10.setForeground(Color.RED);
 		btnNewButton_10.setFont(new Font("Imprint MT Shadow", Font.BOLD, 35));
 		btnNewButton_10.setBounds(279, 159, 106, 39);
-		frame.getContentPane().add(btnNewButton_10);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_10);
 		
 		JButton btnNewButton_11 = new JButton("\u00D7");
 		btnNewButton_11.addActionListener(new ActionListener() {
@@ -224,7 +227,7 @@ public class kelabandor {
 		btnNewButton_11.setForeground(Color.BLUE);
 		btnNewButton_11.setBackground(Color.ORANGE);
 		btnNewButton_11.setBounds(279, 209, 48, 40);
-		frame.getContentPane().add(btnNewButton_11);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_11);
 		
 		JButton btnNewButton_10_1 = new JButton("=");
 		btnNewButton_10_1.addActionListener(new ActionListener() {
@@ -271,7 +274,7 @@ public class kelabandor {
 		btnNewButton_10_1.setFont(new Font("Imprint MT Shadow", Font.BOLD, 99));
 		btnNewButton_10_1.setBackground(Color.ORANGE);
 		btnNewButton_10_1.setBounds(279, 260, 106, 50);
-		frame.getContentPane().add(btnNewButton_10_1);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_10_1);
 		
 		JButton btnNewButton_11_1 = new JButton("\u00F7");
 		btnNewButton_11_1.addActionListener(new ActionListener() {
@@ -285,7 +288,7 @@ public class kelabandor {
 		btnNewButton_11_1.setFont(new Font("Imprint MT Shadow", Font.BOLD, 24));
 		btnNewButton_11_1.setBackground(Color.ORANGE);
 		btnNewButton_11_1.setBounds(337, 209, 48, 40);
-		frame.getContentPane().add(btnNewButton_11_1);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_11_1);
 		
 		JButton btnNewButton_5_1 = new JButton("-");
 		btnNewButton_5_1.addActionListener(new ActionListener() {
@@ -299,7 +302,7 @@ public class kelabandor {
 		btnNewButton_5_1.setFont(new Font("Impact", Font.BOLD, 30));
 		btnNewButton_5_1.setBackground(Color.ORANGE);
 		btnNewButton_5_1.setBounds(395, 158, 73, 40);
-		frame.getContentPane().add(btnNewButton_5_1);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_5_1);
 		
 		JButton btnNewButton_5_2 = new JButton("%");
 		btnNewButton_5_2.addActionListener(new ActionListener() {
@@ -313,7 +316,7 @@ public class kelabandor {
 		btnNewButton_5_2.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton_5_2.setBackground(Color.ORANGE);
 		btnNewButton_5_2.setBounds(395, 209, 73, 40);
-		frame.getContentPane().add(btnNewButton_5_2);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_5_2);
 		
 		JButton btnNewButton_5_3 = new JButton(".");
 		btnNewButton_5_3.addActionListener(new ActionListener() {
@@ -326,7 +329,7 @@ public class kelabandor {
 		btnNewButton_5_3.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton_5_3.setBackground(Color.ORANGE);
 		btnNewButton_5_3.setBounds(395, 260, 73, 50);
-		frame.getContentPane().add(btnNewButton_5_3);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_5_3);
 		
 		JButton btnNewButton_5_4 = new JButton("C");
 		btnNewButton_5_4.addActionListener(new ActionListener() {
@@ -338,7 +341,7 @@ public class kelabandor {
 		btnNewButton_5_4.setFont(new Font("Imprint MT Shadow", Font.BOLD, 30));
 		btnNewButton_5_4.setBackground(Color.ORANGE);
 		btnNewButton_5_4.setBounds(395, 109, 73, 38);
-		frame.getContentPane().add(btnNewButton_5_4);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_5_4);
 		
 		JButton btnNewButton_5_5 = new JButton("\uF0e7");
 		btnNewButton_5_5.addActionListener(new ActionListener() {
@@ -357,7 +360,7 @@ public class kelabandor {
 		btnNewButton_5_5.setFont(new Font("wingdings", Font.BOLD, 30));
 		btnNewButton_5_5.setBackground(Color.ORANGE);
 		btnNewButton_5_5.setBounds(395, 58, 73, 40);
-		frame.getContentPane().add(btnNewButton_5_5);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_5_5);
 		
 		JButton btnNewButton_5_6 = new JButton("WN");
 		btnNewButton_5_6.addActionListener(new ActionListener() {
@@ -370,6 +373,6 @@ public class kelabandor {
 		btnNewButton_5_6.setFont(new Font("Imprint MT Shadow", Font.BOLD, 20));
 		btnNewButton_5_6.setBackground(Color.ORANGE);
 		btnNewButton_5_6.setBounds(395, 11, 73, 40);
-		frame.getContentPane().add(btnNewButton_5_6);
+		frmKelaBandorCalculator.getContentPane().add(btnNewButton_5_6);
 	}
 }
